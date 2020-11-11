8dfa31e4ff3702db102c635476a0431a3dd72bca
Avinash Tuscano <avinashtuscano@gmail.com>
2020-11-11 22:49:01 +0000

    Initial Project Version

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+function multiplyTwo(num1, num2)
+{
+    return num1*num2;
+
+}
+
+console.log(multiplyTwo(6,4));
+
+const multiplyTwoAgain = (num1, num2) => num1*num2;
+console.log(multiplyTwoAgain(6,4));
+
+function print()
+{
+    return "hello world";
+
+}
+console.log(print());
+
+const printAgain = () => "hello world";
+console.log(printAgain);    
